0c5c54cac16f444bcdb44b6c57a368aee439b785
Turețchi Gabriel <g.turetchi2001@gmail.com>
2025-01-27 23:07:33 +0200

    Animated button

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,8 +12,30 @@ export default function Index() {
   return (
     <>
       <div className="flex justify-center items-center h-screen">
-        <button className="box p-16">Hello World</button>
+        <button className="download-button box p-2 text-cyan-50">
+          {ArrowDown}
+          {ArrowDown}
+        </button>
       </div>
     </>
   );
 }
+
+const ArrowDown = (
+  <svg
+    width="18"
+    height="18"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className="arrow"
+  >
+    <path
+      d="M18.25 14L12 20.25L5.75 14M12 19.5V3.75"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
