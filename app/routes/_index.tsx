30577453a94ef71b635d91f8186314a14898c65e
Turețchi Gabriel <g.turetchi2001@gmail.com>
2025-02-03 21:59:46 +0200

    Create a simple line :)

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -33,22 +33,23 @@ export default function Index() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     mountRef.current.appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    camera.position.z = 5;
+    camera.position.set(0, 0, 100);
+    camera.lookAt(0, 0, 0);
 
-    const animate = () => {
-      requestAnimationFrame(animate);
+    const materialLine = new THREE.LineBasicMaterial({ color: 0x0000ff });
 
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+    const points = [];
+    points.push(new THREE.Vector3(-10, 0, 0));
+    points.push(new THREE.Vector3(0, 10, 0));
+    points.push(new THREE.Vector3(10, 0, 0));
 
-      renderer.render(scene, camera);
-    };
+    const geometryLine = new THREE.BufferGeometry().setFromPoints(points);
 
-    animate();
+    const line = new THREE.Line(geometryLine, materialLine);
+
+    scene.add(line);
+
+    renderer.render(scene, camera);
 
     return () => {
       renderer.dispose();
