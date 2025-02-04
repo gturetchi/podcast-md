0d958305fa038281d3bd4e2fa8edfce52ff60f09
Turețchi Gabriel <g.turetchi2001@gmail.com>
2025-02-04 10:05:42 +0200

    Create a cubic cloud

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import { useEffect, useRef } from "react";
 import "public/styles.css";
-import { renderer } from "react-dom";
 
 import * as THREE from "three";
 
@@ -22,32 +21,78 @@ export default function Index() {
     if (!mountRef.current) return;
 
     const scene = new THREE.Scene();
+    scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
     const camera = new THREE.PerspectiveCamera(
-      75,
+      60,
       window.innerWidth / window.innerHeight,
-      0.1,
-      1000
+      1,
+      10000
     );
 
-    const renderer = new THREE.WebGLRenderer();
+    camera.position.x = 0;
+    camera.position.z = 200;
+    camera.position.y = 100;
+
+    const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
     mountRef.current.appendChild(renderer.domElement);
 
-    camera.position.set(0, 0, 100);
-    camera.lookAt(0, 0, 0);
+    //LIGHT SETUP
+    const hemisphereLight = new THREE.HemisphereLight(0xaaaaaa, 0x000000, 0.9);
+    const shadowLight = new THREE.DirectionalLight(0xffffff, 0.9);
+    shadowLight.position.set(150, 350, 350);
+    shadowLight.castShadow = true;
+    shadowLight.shadow.camera.left = -400;
+    shadowLight.shadow.camera.right = 400;
+    shadowLight.shadow.camera.top = 400;
+    shadowLight.shadow.camera.bottom = -400;
+    shadowLight.shadow.camera.near = 1;
+    shadowLight.shadow.camera.far = 1000;
 
-    const materialLine = new THREE.LineBasicMaterial({ color: 0x0000ff });
+    shadowLight.shadow.mapSize.width = 2048;
+    shadowLight.shadow.mapSize.height = 2048;
 
-    const points = [];
-    points.push(new THREE.Vector3(-10, 0, 0));
-    points.push(new THREE.Vector3(0, 10, 0));
-    points.push(new THREE.Vector3(10, 0, 0));
+    scene.add(hemisphereLight);
+    scene.add(shadowLight);
 
-    const geometryLine = new THREE.BufferGeometry().setFromPoints(points);
+    const geom = new THREE.CylinderGeometry(600, 600, 800, 40, 10);
+    geom.applyMatrix4(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
+    const mat = new THREE.MeshPhongMaterial({
+      color: 0x68c3c0,
+      transparent: true,
+      opacity: 0.6,
+      shading: THREE.FlatShading,
+    });
 
-    const line = new THREE.Line(geometryLine, materialLine);
+    const ground = new THREE.Mesh(geom, mat);
+    ground.receiveShadow = true;
+    ground.position.y = -600;
 
-    scene.add(line);
+    scene.add(ground);
+
+    //RENDER CLOUDS
+    const geomCloud = new THREE.BoxGeometry(20, 20, 20);
+    const matCloud = new THREE.MeshPhongMaterial({ colors: 0xffffff });
+
+    const nClouds = 3 + Math.floor(Math.random() * 3);
+
+    for (let i = 0; i < nClouds; i++) {
+      const m = new THREE.Mesh(geomCloud, matCloud);
+      m.position.x = i * 15;
+      m.position.y = Math.random() * 10;
+      m.position.z = Math.random() * 10;
+      m.rotation.z = Math.random() * Math.PI * 2;
+      m.rotation.y = Math.random() * Math.PI * 2;
+
+      const s = 0.1 + Math.random() * 0.9;
+      m.scale.set(s, s, s);
+
+      m.castShadow = true;
+      m.receiveShadow = true;
+
+      scene.add(m);
+    }
 
     renderer.render(scene, camera);
 
@@ -57,9 +102,5 @@ export default function Index() {
     };
   }, []);
 
-  return (
-    <div className="flex justify-center items-center h-screen w-screen">
-      <div ref={mountRef} className="absolute top-0 left-0 w-full h-full" />
-    </div>
-  );
+  return <div ref={mountRef} className="box h-screen w-screen"></div>;
 }
