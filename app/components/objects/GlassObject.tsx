3eb079818bfc5287092079bcd06b954779700d26
Turețchi Gabriel <g.turetchi2001@gmail.com>
2025-02-16 20:26:31 +0200

    Added background, glass effect

--- app/components/objects/GlassObject.tsx
+++ app/components/objects/GlassObject.tsx
@@ -1,16 +1,24 @@
 import * as THREE from "three";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
 
 export class GlassObject {
   mesh: THREE.Object3D;
 
   constructor() {
+    const hdrEquirect = new RGBELoader().load(
+      "https://picsum.photos/id/11/1920/1080",
+      () => {
+        hdrEquirect.mapping = THREE.EquirectangularReflectionMapping;
+      },
+    );
     const geom = new THREE.IcosahedronGeometry(1, 0);
 
-    const materials = new THREE.MeshPhysicalMaterial({
-      roughness: 0.2,
+    const material = new THREE.MeshPhysicalMaterial({
+      roughness: 0,
       transmission: 1,
       thickness: 0.5,
+      envMap: hdrEquirect,
     });
-    this.mesh = new THREE.Mesh(geom, materials);
+    this.mesh = new THREE.Mesh(geom, material);
   }
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,6 +2,9 @@ import type { MetaFunction } from "@remix-run/node";
 import { useEffect, useRef } from "react";
 import "public/styles.css";
 
+import * as THREE from "three";
+import { GlassObject } from "app/components/objects/GlassObject";
+
 export const meta: MetaFunction = () => {
   return [
     { title: "podcaST" },
@@ -13,5 +16,75 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  return <div className="box h-screen w-screen">PodcaST</div>;
+  const mountRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!mountRef.current) return;
+
+    const scene = new THREE.Scene();
+    scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
+    const camera = new THREE.PerspectiveCamera(
+      60,
+      window.innerWidth / window.innerHeight,
+      1,
+      10000,
+    );
+
+    camera.position.x = 0;
+    camera.position.z = 200;
+    camera.position.y = 100;
+
+    const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+    mountRef.current.appendChild(renderer.domElement);
+
+    renderer.physicallyCorrectLights = true;
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.toneMappingExposure = 1.0;
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+    //LIGHT SETUP
+    const hemisphereLight = new THREE.HemisphereLight(0xb1e1ff, 0xb97a20, 0.5);
+    const shadowLight = new THREE.DirectionalLight(0xffffff, 0.9);
+    shadowLight.position.set(150, 350, 350);
+    shadowLight.castShadow = true;
+    shadowLight.shadow.camera.left = -400;
+    shadowLight.shadow.camera.right = 400;
+    shadowLight.shadow.camera.top = 400;
+    shadowLight.shadow.camera.bottom = -400;
+    shadowLight.shadow.camera.near = 1;
+    shadowLight.shadow.camera.far = 1000;
+
+    shadowLight.shadow.mapSize.width = 2048;
+    shadowLight.shadow.mapSize.height = 2048;
+
+    scene.add(hemisphereLight);
+    scene.add(shadowLight);
+
+    const glassObject = new GlassObject();
+    glassObject.mesh.position.set(0, 100, 50);
+    glassObject.mesh.scale.set(50, 50, 50);
+    scene.add(glassObject.mesh);
+
+    function loop() {
+      renderer.render(scene, camera);
+      requestAnimationFrame(loop);
+    }
+
+    loop();
+
+    return () => {
+      renderer.dispose();
+      mountRef.current?.removeChild(renderer.domElement);
+    };
+  }, []);
+
+  return (
+    <div className="h-screen w-screen">
+      <div ref={mountRef} className="h-screen w-screen"></div>;
+    </div>
+  );
 }
