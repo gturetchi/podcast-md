cdfa6acf37842d627ba80cd2be5ae451d0acc1bd
Turețchi Gabriel <g.turetchi2001@gmail.com>
2025-02-18 21:24:01 +0200

    Code clean

--- app/components/objects/GlassObject.tsx
+++ app/components/objects/GlassObject.tsx
@@ -5,12 +5,6 @@ export class GlassObject {
   mesh: THREE.Object3D;
 
   constructor() {
-    const hdrEquirect = new RGBELoader().load(
-      "https://picsum.photos/id/11/1920/1080",
-      () => {
-        hdrEquirect.mapping = THREE.EquirectangularReflectionMapping;
-      },
-    );
     const geom = new THREE.IcosahedronGeometry(1, 0);
 
     const material = new THREE.MeshPhysicalMaterial({
